3580aaa6c0635564f03832ba49e9b598ec3c1e24
AleksandrPasnov <aleksandrpasnov@gmail.com>
2022-09-21 17:37:49 +0300

    py-11

--- PY_11_Работа с Pandas/data_PY_11.py
+++ PY_11_Работа с Pandas/data_PY_11.py
@@ -3,7 +3,18 @@ import pandas as pd
 melb_data = pd.read_csv(
     '/Users/MacBook/Desktop/Data Saintist/IDE/PY_11_Работа с Pandas/data_csv/melb_data_ps.csv', sep=',')
 
-melb_df = melb_data()
+melb_df = melb_data.copy()
+# print(melb_data.head)
+# print()
 
-if __name__ == '__main__':
-    print(melb_data.head)
+melb_df = melb_df.drop(['index', 'Coordinates'], axis=1)
+# melb_df.drop(['index','Coordinates'],axis=1,inplace=True) алтернативный вариант
+
+# print(melb_df.head())
+# print()
+
+total_rooms = melb_df['Rooms'] + melb_df['Bedroom'] + melb_df['Bathroom']
+melb_df['MeanRoomSquare'] = melb_df['BuildingArea']/total_rooms
+
+price_square = melb_df['Price'] **2
+print(price_square)
