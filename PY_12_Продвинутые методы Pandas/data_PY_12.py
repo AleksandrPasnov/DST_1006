db28197549bb411092c6b48a75d15dde179c0c39
AleksandrPasnov <aleksandrpasnov@gmail.com>
2022-10-09 12:13:36 +0300

    12

---
+++ PY_12_Продвинутые методы Pandas/data_PY_12.py
@@ -0,0 +1,5 @@
+import pandas as pd
+melb_df = pd.read_csv(
+    '/Users/MacBook/Desktop/Data Saintist/IDE/PY_12_Продвинутые методы Pandas/data_csv/melb_data_fe.csv')
+print(melb_df.head())
+print(melb_df.info())
